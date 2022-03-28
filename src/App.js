7764d3476a5d8a45d0f1420a6713dfe8626d91ba
Rabi Shankar Prasad <rabiprasad607@gmail.com>
2022-03-29 00:26:55 +0530

    error page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Form from "./components/pages/form/Form";
+import Error from "./components/pages/error/Error";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="" element={<Form />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
