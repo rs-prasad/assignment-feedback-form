b85f732d34bb05e85d6f420d76fe11ff7933c40f
Rabi Shankar Prasad <rabiprasad607@gmail.com>
2022-03-29 00:14:57 +0530

    form functionality added

--- src/components/pages/form/Form.js
+++ src/components/pages/form/Form.js
@@ -1,24 +1,76 @@
 import "./form.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Form = () => {
   const ratingOptions = ["Excellent", "Good", "Fair", "Bad"];
 
-  const [personalDetail, setPersonalDetail] = useState({
+  const [feedback, setFeedback] = useState({
     name: "",
     email: "",
     phoneNumber: "",
+    service: "",
+    beverage: "",
+    cleanness: "",
+    overall: "",
   });
+  const [error, setError] = useState({});
 
-  const handelChange = (e) => {
+  const saveData = () => {
+    let feedbackArray = JSON.parse(
+      localStorage.getItem("feedbackArray") || "[]"
+    );
+    console.log(typeof feedbackArray);
+    feedbackArray = [...feedbackArray, feedback];
+    localStorage.setItem("feedbackArray", JSON.stringify(feedbackArray));
+  };
+  //handles
+  const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setPersonalDetail({ ...personalDetail, [name]: value });
+    setFeedback({ ...feedback, [name]: value });
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(document.forms[0]);
+    //validation
+    const newErrorMessage = {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      service: "",
+      beverage: "",
+      cleanness: "",
+      overall: "",
+    };
+
+    let anyError = false;
+    const regexName = /^[a-zA-Z\s]{1,}$/;
+    const regexPhoneNumber = /^[0-9]{10}$/;
+    const regexEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z]{3,}.[a-zA-Z]{2,3}$/;
+
+    if (!regexName.test(feedback.name)) {
+      newErrorMessage.name =
+        "Name fields should be valid and properly formatted.";
+      anyError = true;
+    }
+    if (!regexEmail.test(feedback.email)) {
+      newErrorMessage.email = "Email should be valid and properly formatted.";
+      anyError = true;
+    }
+
+    if (!regexPhoneNumber.test(feedback.phoneNumber)) {
+      newErrorMessage.phoneNumber =
+        "Phone number fields should be valid and properly formatted.";
+      anyError = true;
+    }
+
+    for (const key in feedback) {
+      if (!feedback[key]) {
+        newErrorMessage[key] = "All fields are mandatory";
+        anyError = true;
+      }
+    }
+    setError(newErrorMessage);
+    if (!anyError) saveData();
   };
 
   return (
@@ -45,21 +97,23 @@ const Form = () => {
               type="text"
               name="name"
               id="form__name__input"
-              value={personalDetail.name}
-              onChange={(e) => handelChange(e)}
+              value={feedback.name}
+              onChange={(e) => handleChange(e)}
             />
+            {error.name && <p className="error__message">{error.name}</p>}
           </fieldset>
           <fieldset className="form__email">
             <label htmlFor="form__email__input" className="form__email__title">
               Email
             </label>
             <input
-              type="email"
+              type="text"
               name="email"
               id="form__email__input"
-              value={personalDetail.email}
-              onChange={(e) => handelChange(e)}
+              value={feedback.email}
+              onChange={(e) => handleChange(e)}
             />
+            {error.email && <p className="error__message">{error.email}</p>}
           </fieldset>
           <fieldset className="form__phone">
             <label htmlFor="form__phone__input" className="form__phone__title">
@@ -70,9 +124,12 @@ const Form = () => {
               name="phoneNumber"
               id="form__phone__input"
               placeholder="Don't include +91"
-              value={personalDetail.phoneNumber}
-              onChange={(e) => handelChange(e)}
+              value={feedback.phoneNumber}
+              onChange={(e) => handleChange(e)}
             />
+            {error.phoneNumber && (
+              <p className="error__message">{error.phoneNumber}</p>
+            )}
           </fieldset>
         </div>
         {/* Ratings */}
@@ -84,54 +141,62 @@ const Form = () => {
               Please rate the quality of the service you received from your
               host.
             </label>
-            <div className="rating_options">
-              {ratingOptions.map((item) => (
-                <>
+            <div className="rating_options" onChange={(e) => handleChange(e)}>
+              {ratingOptions.map((item, index) => (
+                <span key={index}>
                   <input type="radio" name="service" id="" value={item} />
-                  <span>{item}</span>
-                </>
+                  <span className="rating__title">{item}</span>
+                </span>
               ))}
             </div>
+            {error.service && <p className="error__message">{error.service}</p>}
           </fieldset>
           {/* beverage */}
           <fieldset className="rating__beverage">
             <label htmlFor="beverage">
               Please rate the quality of your beverage.
             </label>
-            <div className="rating_options">
-              {ratingOptions.map((item) => (
-                <>
+            <div className="rating_options" onChange={(e) => handleChange(e)}>
+              {ratingOptions.map((item, index) => (
+                <span key={index}>
                   <input type="radio" name="beverage" id="" value={item} />
-                  <span>{item}</span>
-                </>
+                  <span className="rating__title">{item}</span>
+                </span>
               ))}
             </div>
+            {error.beverage && (
+              <p className="error__message">{error.beverage}</p>
+            )}
           </fieldset>
           {/* cleanness */}
           <fieldset className="rating__cleanness">
             <label htmlFor="cleanness">Was our restaurant clean?</label>
-            <div className="rating_options">
-              {ratingOptions.map((item) => (
-                <>
+            <div className="rating_options" onChange={(e) => handleChange(e)}>
+              {ratingOptions.map((item, index) => (
+                <span key={index}>
                   <input type="radio" name="cleanness" id="" value={item} />
-                  <span>{item}</span>
-                </>
+                  <span className="rating__title">{item}</span>
+                </span>
               ))}
             </div>
+            {error.cleanness && (
+              <p className="error__message">{error.cleanness}</p>
+            )}
           </fieldset>
           {/* overall */}
           <fieldset className="rating__overall">
             <label htmlFor="overall">
               Please rate your overall dining experience.
             </label>
-            <div className="rating_options">
-              {ratingOptions.map((item) => (
-                <>
+            <div className="rating_options" onChange={(e) => handleChange(e)}>
+              {ratingOptions.map((item, index) => (
+                <span key={index}>
                   <input type="radio" name="overall" id="" value={item} />
-                  <span>{item}</span>
-                </>
+                  <span className="rating__title">{item}</span>
+                </span>
               ))}
             </div>
+            {error.overall && <p className="error__message">{error.overall}</p>}
           </fieldset>
         </div>
         <div className="submit_btn_container">
